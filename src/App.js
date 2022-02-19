c7eac6184e3bf40efda186d2f6b8f3d6074de844
Yulin <yulinli@Yulins-MacBook-Pro-2.local>
2022-02-19 15:18:00 -0800

    set up and initial progress (10 mins into tutorial)

--- src/App.js
+++ src/App.js
@@ -1,25 +1,38 @@
-import logo from './logo.svg';
+import React, {useEffect, useState} from 'react';
+import Recipe from './Recipe'
 import './App.css';
 
-function App() {
-  return (
+
+// class App extends Component {
+//   render() {
+//     return (
+//       <div className = "App">
+//         <h1>Hello React</h1>
+//       </div>
+//     )
+//   }
+// }
+const App = () => {
+
+  const APP_ID = 'b26f7b04';
+  const APP_KEY = '148c9ad2b23938f025de88ab55a6b918';
+  const exampleReq = `https://api.edamam.com/api/recipes/v2?type=public&q=chicken&app_id=${APP_ID}&app_key=${APP_KEY}`;
+
+  const [counter, setCounter] = useState(0);
+
+  useEffect(()=>{
+    console.log('effect has been run');
+  })
+  
+  return(
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <form className = "search-form">
+        <input className = "search-bar" type = 'text'/>
+        <button className = "search-button" type = 'submit'>search</button>
+      </form>
+      <h1 onClick = {()=> setCounter(counter + 1)}>{counter}</h1>
     </div>
-  );
+  )
 }
 
 export default App;
---
+++ src/Recipe.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+
+const Recipe = () => {
+    return (
+        <div>
+            <h1>Title</h1>
+            <p>Calories</p>
+            <image></image>
+        </div>
+
+    );
+}
+
+export default Recipe;
